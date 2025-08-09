ea96133381f9af101776e74e46a214b4d47afe5b
Tyson Whitehead <twhitehead@gmail.com>
2025-08-09 09:06:26 -0400

    Clean up questions, add log file, and clarify system
    Program now creates a nice ffdefault.log to submit with all
    details so no issue with people accidentally reporting things wrong
    or not reporting all the desired details.
    
    Also asks about wine version so me know if it was before or after
    merge request 8605 stripped autocenter support from the udev input
    and SDL backends.

--- ffdefault.c
+++ ffdefault.c
@@ -8,12 +8,14 @@
 
 
 char message[100];
+FILE* logfile;
 IDirectInput8 *dinput;
 HWND hwindow;
 
 
 BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *context) {
   HRESULT hr;
+  int yesno;
   IDirectInputDevice8A *device;
   DIPROPDWORD autocenter = {
     .diph = {
@@ -22,6 +24,12 @@ BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *cont
       .dwHow = DIPH_DEVICE,
     },
   };
+
+  // Log device
+  sprintf_s(message, sizeof(message)/sizeof(message[0]),
+            "FFDEFAULT: Product name: %s\n", instance->tszProductName);
+  if (logfile) fputs(message, logfile);
+  OutputDebugString(message);
 
   // Required setup stuff
   hr = IDirectInput8_CreateDevice(dinput, &instance->guidInstance, &device, 0);
@@ -63,17 +71,24 @@ BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *cont
   }
 
   sprintf_s(message, sizeof(message)/sizeof(message[0]),
-            "DIPROP_AUTOCENTER initial value: %s\n",
-            autocenter.dwData == DIPROPAUTOCENTER_ON ? "DIPROPAUTOCENTER_ON" :
-            autocenter.dwData == DIPROPAUTOCENTER_OFF ? "DIPROPAUTOCENTER_OFF" :
-            "unknown value");
+            "FFDEFAULT: Autocenter initial value: %s\n",
+            autocenter.dwData == DIPROPAUTOCENTER_ON ? "on" :
+            autocenter.dwData == DIPROPAUTOCENTER_OFF ? "off" :
+            "other");
+  if (logfile) fputs(message, logfile);
+  OutputDebugString(message);
+  sprintf_s(message, sizeof(message)/sizeof(message[0]),
+            "Autocenter initial value: %s\n",
+            autocenter.dwData == DIPROPAUTOCENTER_ON ? "on" :
+            autocenter.dwData == DIPROPAUTOCENTER_OFF ? "off" :
+            "other");
   MessageBox(hwindow, message, instance->tszProductName, MB_OK);
 
-  // Aquire with default value
+  // Acquire with default value
   hr = IDirectInputDevice8_Acquire(device);
   if (FAILED(hr)) {
     sprintf_s(message, sizeof(message)/sizeof(message[0]),
-              "Unable to aquire direct input device: hr %#lx\n", hr);
+              "Unable to acquire direct input device: hr %#lx\n", hr);
     OutputDebugString(message);
     MessageBox(hwindow, message, "Error", MB_OK);
     IDirectInputDevice_Release(device);
@@ -81,20 +96,24 @@ BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *cont
   }
 
   sprintf_s(message, sizeof(message)/sizeof(message[0]),
-            "Aquired without first setting DIPROP_AUTOCENTER\n");
-  MessageBox(hwindow, message, instance->tszProductName, MB_OK);
-  
+            "Acquired without setting autocentering.\n\nIs your device autocentering?");
+  yesno = MessageBox(hwindow, message, instance->tszProductName, MB_YESNO);
+  sprintf_s(message, sizeof(message)/sizeof(message[0]),
+            "FFDEFAULT: Acquired with default: %s\n", yesno == IDYES ? "autocentering" : "not autocentering");
+  if (logfile) fputs(message, logfile);
+  OutputDebugString(message);
+
   hr = IDirectInputDevice8_Unacquire(device);
   if (FAILED(hr)) {
     sprintf_s(message, sizeof(message)/sizeof(message[0]),
-              "Unable to unaquire direct input device: hr %#lx\n", hr);
+              "Unable to unacquire direct input device: hr %#lx\n", hr);
     OutputDebugString(message);
     MessageBox(hwindow, message, "Error", MB_OK);
     IDirectInputDevice_Release(device);
     return DIENUM_CONTINUE;
   }
 
-  // Aquire with DIPROPAUTOCENTER_OFF
+  // Acquire with DIPROPAUTOCENTER_OFF
   autocenter.dwData = DIPROPAUTOCENTER_OFF;
   hr = IDirectInputDevice8_SetProperty(device, DIPROP_AUTOCENTER, &autocenter.diph);
   if (FAILED(hr)) {
@@ -109,7 +128,7 @@ BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *cont
   hr = IDirectInputDevice8_Acquire(device);
   if (FAILED(hr)) {
     sprintf_s(message, sizeof(message)/sizeof(message[0]),
-              "Unable to aquire direct input device: hr %#lx\n", hr);
+              "Unable to acquire direct input device: hr %#lx\n", hr);
     OutputDebugString(message);
     MessageBox(hwindow, message, "Error", MB_OK);
     IDirectInputDevice_Release(device);
@@ -117,20 +136,24 @@ BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *cont
   }
 
   sprintf_s(message, sizeof(message)/sizeof(message[0]),
-            "Aquired after setting DIPROP_AUTOCENTER to DIPROPAUTOCENTER_OFF\n");
-  MessageBox(hwindow, message, instance->tszProductName, MB_OK);
-  
+            "Acquired after setting autocentering off.\n\nIs your device autocentering?");
+  yesno = MessageBox(hwindow, message, instance->tszProductName, MB_YESNO);
+  sprintf_s(message, sizeof(message)/sizeof(message[0]),
+            "FFDEFAULT: Acquired with off: %s\n", yesno == IDYES ? "autocentering" : "not autocentering");
+  if (logfile) fputs(message, logfile);
+  OutputDebugString(message);
+
   hr = IDirectInputDevice8_Unacquire(device);
   if (FAILED(hr)) {
     sprintf_s(message, sizeof(message)/sizeof(message[0]),
-              "Unable to unaquire direct input device: hr %#lx\n", hr);
+              "Unable to unacquire direct input device: hr %#lx\n", hr);
     OutputDebugString(message);
     MessageBox(hwindow, message, "Error", MB_OK);
     IDirectInputDevice_Release(device);
     return DIENUM_CONTINUE;
   }
-    
-  // Aquire with DIPROPAUTOCENTER_ON
+
+  // Acquire with DIPROPAUTOCENTER_ON
   autocenter.dwData = DIPROPAUTOCENTER_ON;
   hr = IDirectInputDevice8_SetProperty(device, DIPROP_AUTOCENTER, &autocenter.diph);
   if (FAILED(hr)) {
@@ -144,25 +167,29 @@ BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *cont
 
   hr = IDirectInputDevice8_Acquire(device);
   if (FAILED(hr)) {
-    printf("  Unable to aquire direct input device: hr %#lx\n", hr);
+    printf("  Unable to acquire direct input device: hr %#lx\n", hr);
     IDirectInputDevice_Release(device);
     return DIENUM_CONTINUE;
   }
 
   sprintf_s(message, sizeof(message)/sizeof(message[0]),
-            "Aquired after setting DIPROP_AUTOCENTER to DIPROPAUTOCENTER_ON");
-  MessageBox(hwindow, message, instance->tszProductName, MB_OK);
-  
+            "Acquired after setting autocentering on.\n\nIs your device autocentering?");
+  yesno = MessageBox(hwindow, message, instance->tszProductName, MB_YESNO);
+  sprintf_s(message, sizeof(message)/sizeof(message[0]),
+            "FFDEFAULT: Acquired with on: %s\n", yesno == IDYES ? "autocentering" : "not autocentering");
+  if (logfile) fputs(message, logfile);
+  OutputDebugString(message);
+
   hr = IDirectInputDevice8_Unacquire(device);
   if (FAILED(hr)) {
     sprintf_s(message, sizeof(message)/sizeof(message[0]),
-              "Unable to unaquire direct input device: hr %#lx\n", hr);
+              "Unable to unacquire direct input device: hr %#lx\n", hr);
     OutputDebugString(message);
     MessageBox(hwindow, message, "Error", MB_OK);
     IDirectInputDevice_Release(device);
     return DIENUM_CONTINUE;
   }
-    
+
   // Release and done
   IDirectInputDevice_Release(device);
   return DIENUM_CONTINUE;
@@ -172,6 +199,7 @@ BOOL CALLBACK enum_devices_callback(const DIDEVICEINSTANCE *instance, void *cont
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
   HRESULT hr;
 
+  // Create window so we have a window handle for dinput
   hwindow = CreateWindow("STATIC", TEXT("FF Default"), WS_OVERLAPPED | WS_CAPTION,
                          CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, \
                          0, 0, hInstance, 0);
@@ -181,7 +209,46 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
     OutputDebugString(message);
     return 1;
   }
-  
+
+  // Open and note log file
+  logfile = fopen("ffdefault.log", "a");
+  fputs("FFDEFAULT: ==========\n", logfile);
+  if (logfile) {
+    sprintf_s(message, sizeof(message)/sizeof(message[0]),
+              "Please sumbit log file: %s\n", "ffdefault.log");
+    MessageBox(hwindow, message, "Send log file", MB_OK);
+  }
+
+  // Query and log system characteristics
+  if (MessageBox(hwindow, "Are you running under Windows?\n", "Identify your OS", MB_YESNO) == IDYES) {
+    fputs("FFDEFAULT: Windows\n", logfile);
+  }
+  else {
+    if (MessageBox(hwindow, "Is you wine 10.2 or older (if unsure, say no)?\n", "Identify Wine version", MB_YESNO) == IDYES) {
+      fputs("FFDEFAULT: Wine version: <= 10.2\n", logfile);
+    }
+    else if (MessageBox(hwindow, "Is your wine 10.3 or newer (if unsure, say no)?\n", "Identify Wine version", MB_YESNO) == IDYES) {
+      fputs("FFDEFAULT: Wine version: >= 10.3\n", logfile);
+    }
+    else {
+      fputs("FFDEFAULT: Wine version: unsure\n", logfile);
+    }
+
+    if (MessageBox(hwindow, "Are you using the SDL backend under wine (if unsure, say no)?\n", "Identify Wine backend", MB_YESNO) == IDYES) {
+      fputs("FFDEFAULT: Wine backend: SDL\n", logfile);
+    }
+    else if (MessageBox(hwindow, "Are you using the udev input backend under wine (if unsure, say no)?\n", "Identify Wine backend", MB_YESNO) == IDYES) {
+      fputs("FFDEFAULT: Wine backend: udev input\n", logfile);
+    }
+    else if (MessageBox(hwindow, "Are you using the udev hidraw backend under wine (if unsure, say no)?\n", "Identify Wine backend", MB_YESNO) == IDYES) {
+      fputs("FFDEFAULT: Wine backend: udev hidraw\n", logfile);
+    }
+    else {
+      fputs("FFDEFAULT: Wine backend: unsure\n", logfile);
+    }
+  }
+
+  // Acquire direct input interface
   hr = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, &IID_IDirectInput8, (void**)&dinput, 0);
   if (FAILED(hr)) {
     sprintf_s(message, sizeof(message)/sizeof(message[0]),
@@ -191,6 +258,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
     return 1;
   }
 
+  // Enumerate FF devices
   hr = IDirectInput8_EnumDevices(dinput, DI8DEVCLASS_ALL, enum_devices_callback, 0, DIEDFL_FORCEFEEDBACK);
   if (FAILED(hr)) {
     sprintf_s(message, sizeof(message)/sizeof(message[0]),
@@ -200,6 +268,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
     return 1;
   }
 
+  // Cleanup
   IDirectInput_Release(dinput);
+
+  if (logfile)
+    fclose(logfile);
+
   return 0;
 }
